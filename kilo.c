d5b1e0ad60ba1efba448e46d95b07679b9f35927
Jaldhar H. Vyas <jaldhar@braincells.com>
2020-11-19 15:50:02 -0500

    Raw Input and Output: clear the screen on exit.

--- kilo.c
+++ kilo.c
@@ -17,6 +17,9 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);
   exit(1);
 }
@@ -57,6 +60,8 @@ void editorProcessKeypress() {
 
   switch (c) {
     case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
   }
