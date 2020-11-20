d6a331e2db545adee92836f9118749d76cac460f
Jaldhar H. Vyas <jaldhar@braincells.com>
2020-11-19 21:49:23 -0500

    Raw Input and Output: step 45.

--- kilo.c
+++ kilo.c
@@ -119,6 +119,23 @@ void abFree(struct abuf *ab) {
 
 /*** input ***/
 
+void editorMoveCursor(char key) {
+  switch (key) {
+    case 'a':
+      E.cx--;
+      break;
+    case 'd':
+      E.cx++;
+      break;
+    case 'w':
+      E.cy--;
+      break;
+    case 's':
+      E.cy++;
+      break;
+  }
+}
+
 void editorProcessKeypress() {
   char c = editorReadKey();
 
@@ -127,6 +144,12 @@ void editorProcessKeypress() {
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
+      break;
+    case 'w':
+    case 's':
+    case 'a':
+    case 'd':
+      editorMoveCursor(c);
       break;
   }
 }
