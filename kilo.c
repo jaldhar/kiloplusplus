73073eb70e61ff8f172d76091f4f5576ba8505fb
Jaldhar H. Vyas <jaldhar@braincells.com>
2020-11-20 16:11:40 -0500

    Raw Input and Output: step 51.

--- kilo.c
+++ kilo.c
@@ -191,6 +191,16 @@ void editorProcessKeypress() {
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
+
+    case PAGE_UP:
+    case PAGE_DOWN:
+      {
+        int times = E.screenrows;
+        while (times--)
+          editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+      }
+      break;
+
     case ARROW_UP:
     case ARROW_DOWN:
     case ARROW_LEFT:
