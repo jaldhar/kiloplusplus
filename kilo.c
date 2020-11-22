0d0eb028a8bd968f72f33e7bee322e6c92989917
Jaldhar H. Vyas <jaldhar@braincells.com>
2020-11-21 20:50:18 -0500

    A Text Viewer: step 71.

--- kilo.c
+++ kilo.c
@@ -40,6 +40,7 @@ typedef struct erow {
 struct editorConfig {
   int cx, cy;
   int rowoff;
+  int coloff;
   int screenrows;
   int screencols;
   int numrows;
@@ -346,6 +347,7 @@ void initEditor() {
   E.cx = 0;
   E.cy = 0;
   E.rowoff = 0;
+  E.coloff = 0;
   E.numrows = 0;
   E.row = NULL;
 
