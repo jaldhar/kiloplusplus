fdfb08d1b88a85056d248bbc3df916c1b2ebc452
Jaldhar H. Vyas <jaldhar@braincells.com>
2020-11-21 21:00:31 -0500

    A Text Viewer: limit scrolling to the right.

--- kilo.c
+++ kilo.c
@@ -217,6 +217,8 @@ void abFree(struct abuf *ab) {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) {
@@ -224,7 +226,9 @@ void editorMoveCursor(int key) {
       }
       break;
     case ARROW_RIGHT:
+      if (row && E.cx < row->size) {
         E.cx++;
+      }
       break;
     case ARROW_UP:
       if (E.cy != 0) {
