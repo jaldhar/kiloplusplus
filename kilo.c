26f9193dc1e31322fb994fbf94c84f8e40fb5a31
Jaldhar H. Vyas <jaldhar@braincells.com>
2020-11-23 16:49:23 -0500

    A Text Editor: step 106.

--- kilo.c
+++ kilo.c
@@ -4,6 +4,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -280,6 +281,19 @@ void editorOpen(const char *filename) {
   }
   free(line);
   fclose(fp);
+}
+
+void editorSave() {
+  if (E.filename == NULL) return;
+
+  int len;
+  char *buf = editorRowsToString(&len);
+
+  int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+  ftruncate(fd, len);
+  write(fd, buf, len);
+  close(fd);
+  free(buf);
 }
 
 /*** append buffer ***/
