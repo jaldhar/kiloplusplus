93edf437fd76483ced26bec201a8c9cd9ce78d4b
Jaldhar H. Vyas <jaldhar@braincells.com>
2020-11-23 22:29:52 -0500

    A Text Editor: step 120.

--- kilo.c
+++ kilo.c
@@ -272,6 +272,15 @@ void editorInsertChar(int c) {
   E.cx++;
 }
 
+void editorRowAppendString(erow *row, char *s, size_t len) {
+  row->chars = realloc(row->chars, row->size + len + 1);
+  memcpy(&row->chars[row->size], s, len);
+  row->size += len;
+  row->chars[row->size] = '\0';
+  editorUpdateRow(row);
+  E.dirty++;
+}
+
 void editorDelChar() {
   if (E.cy == E.numrows) return;
 
