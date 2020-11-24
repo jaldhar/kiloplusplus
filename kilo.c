563a08e4c9abe85f7c5a160d27cee53c836f0188
Jaldhar H. Vyas <jaldhar@braincells.com>
2020-11-23 22:23:05 -0500

    A Text Editor: step 118.

--- kilo.c
+++ kilo.c
@@ -436,7 +436,8 @@ void editorProcessKeypress() {
     case BACKSPACE:
     case CTRL_KEY('h'):
     case DEL_KEY:
-      /* TODO */
+      if (c == DEL_KEY) editorMoveCursor(ARROW_RIGHT);
+      editorDelChar();
       break;
 
     case PAGE_UP:
