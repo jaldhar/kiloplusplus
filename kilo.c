90b8b4d41ab6de33ff488a70d0337a2e3f373f33
Jaldhar H. Vyas <jaldhar@braincells.com>
2020-11-23 22:34:29 -0500

    A Text Editor: step 121.

--- kilo.c
+++ kilo.c
@@ -283,11 +283,17 @@ void editorRowAppendString(erow *row, char *s, size_t len) {
 
 void editorDelChar() {
   if (E.cy == E.numrows) return;
+  if (E.cx == 0 && E.cy == 0) return;
 
   erow *row = &E.row[E.cy];
   if (E.cx > 0) {
     editorRowDelChar(row, E.cx - 1);
     E.cx--;
+  } else {
+    E.cx = E.row[E.cy - 1].size;
+    editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+    editorDelRow(E.cy);
+    E.cy--;
   }
 }
 
