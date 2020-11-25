1963605a8311f40ae8504e6e59685fcf1240fff5
Jaldhar H. Vyas <jaldhar@braincells.com>
2020-11-25 17:42:07 -0500

    Syntax Highlighting: step 170.

--- kilo.c
+++ kilo.c
@@ -54,6 +54,7 @@ enum editorHighlight {
 struct editorSyntax {
   char *filetype;
   char **filematch;
+  char *singleline_comment_start;
   int flags;
 };
 
@@ -91,6 +92,7 @@ struct editorSyntax HLDB[] = {
   {
     "c",
     C_HL_extensions,
+    "//",
     HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
   },
 };
